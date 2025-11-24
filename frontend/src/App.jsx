6e48a470c2843f86d7fb4f8b58e9adef8af6f70d
bistvan4 <bencze780@gmail.com>
2025-11-24 21:44:59 +0100

    statikus web

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import './index.css';
-function App() {
-  return (   
-    <div>
-    
-      <h1>Üdvözöllek az React-ban!</h1>
-      <p>Ez az APP a főoldal!</p>  
-    
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [htmlContent, setHtmlContent] = useState('');
+
+  useEffect(() => {
+    // Lekérjük a fájlt a public mappából
+    fetch('/index.html') 
+      .then(response => {
+        if (!response.ok) throw new Error("Nem sikerült betölteni");
+        return response.text(); // Szövegként olvassuk be
+      })
+      .then(data => setHtmlContent(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Beolvasztott HTML tartalom</h1>
+      
+      {/* Itt jelenik meg a fájl tartalma */}
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
